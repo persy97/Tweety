949d5d4ead62954a516d4407cb2ba801978e66cc
Prashant Kumar Upadhyay <prashant10723@gmail.com>
2018-06-10 16:44:24 +0530

    Keys Hidden

--- filter/secretkeys.py
+++ filter/secretkeys.py
@@ -1,5 +1,5 @@
-ckey = "ZtHWuNfHCWV3XqmFSuXv0FyKn"
-csecret = "gmgepICbMUVtnmePAa6ibcr93r89b7zzx72wfCWhonyJlbbWm7"
+ckey = #Enter your Consumer Key
+csecret = #Enter your Consumer Secret
 
-akey = "151769085-yx4BQMhM3rrRnZ7B7pnWcXwVIBu2uXwx8AD3vigc"
-asecret =	"r969t4xTfgLigwD1oh1JHkv91ArYUMNmry7MDnGyEsKOk"
+akey = #Enter Access Key
+asecret =	#Enter Access Secret
